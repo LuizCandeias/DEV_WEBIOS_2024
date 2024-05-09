c0357eabf28c63bedd075da7ad63ca4f7f7959ca
LuizCandeias <Luizway.54@gmail.com>
2024-05-08 21:43:22 -0300

    JS02-Variaveis

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,46 @@
+console.log("Olá Mundo!")
+
+// Variaveis
+//Quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria ira receber o valor 8
+
+var _felippe = 8
+_felippe = "Luiz +12" // redeclarar uma variavel
+console.log(_felippe) 
+
+let letsGo = 4 // Não posso redeclarar
+letsGo = 6
+console.log(letsGo)
+
+console.clear()
+
+// Const
+const PI = 3.1415
+console.log(PI)
+
+let NAW = 0 / 0
+console.log(NAW)
+
+console.clear()
+
+let IOS = "Instituto da oportunidade social" // variavel corinthias recebeu uma string
+console.log("Insituto " + "da " + "oportunidade " + "social")
+
+var numero1 = 2
+var texto = "Eu tenho"
+var texto2 = "reais"
+console.log( "eu tenho " , "2 " , "reais ")
+// concatenar = é possivel concatenar strings ou concatenar variaveis
+
+console.clear()
+
+// Valores booleanos
+
+var num1 = 5
+var num2 = 5
+//num1 é igual a num2
+// 1 é igual a 1 = true
+
+console.log( num1 == num2)
+
+var teste;
+console.log(teste)
