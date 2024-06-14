5f73124580c421ec996d1b370371c5d9bda1916b
LuizCandeias <Luizway.54@gmail.com>
2024-06-14 20:00:56 -0300

    JS

--- JS/JS-15-API-Parte1/main.js
+++ JS/JS-15-API-Parte1/main.js
@@ -7,18 +7,20 @@ function getImage(e) {
     fetch(url_api, {
     method: 'GET',
     })
-    .then((response) => {
-    return response.json();
-    })
-    .then((data) => {
-    // console.log(data);
-    document.getElementById('json_aqui').innerText =
-    JSON.stringify(data);
-    // console.log(data.message)
-    let imagem = `
-    <img class="rounded img-fluid" src="${data.message}" />
-    `;
+    .then((response) => {return response.json();}).then((data) => 
+    {
+        console.log(data);
+        document.getElementById('json_aqui').innerText =
+        JSON.stringify(data);
+        console.log(data.message)
+        let imagem = `
+        <img class="rounded img-fluid" src="${data.message}" />
+        `;
     // Insere a imagem no elemento HTML com id imagem_aqui
-    document.querySelector('#imagem_aqui').innerHTML = imagem;
+        document.querySelector('#imagem_aqui').innerHTML = imagem;
     });
     }
+
+    // quando for usar API sempre usar o fetch!!
+
+    // fetch(URL).then(receber o JSON).catch(Tratar o JSON e mostrar as informações em tela para o usuário).catch(catch é para tratar erros retornados pela API)
